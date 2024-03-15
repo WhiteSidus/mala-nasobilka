1a109ccc4002e87cf3567f573d4cf0a631163881
Whiteside <vovadxc@gmail.com>
2024-03-15 12:24:56 +0100

    code pro vypočitaní ruznich tych nevim čeho

---
+++ vyber_bez_vyberu.py
@@ -0,0 +1,45 @@
+import random
+
+body = 0
+
+def nasobeni(a,b):
+    vysledek = a*b
+    return vysledek
+
+def deleni(a,b):
+    vysledek = a/b
+    return vysledek
+
+def scitani(a,b):
+    vysledek = a+b
+    return vysledek
+
+def mocneni(a,b):
+    vysledek = a**b
+    return vysledek
+
+def kontrola(vysledek, vysledek_zak):
+    global body
+    if vysledek == vysledek_zak:
+        print("Jsi šikulka :)")
+        body += 1
+        print(body)
+    else:
+        print("Jejda, spletl jsi se odpovedí :(")
+        body -= 1
+        print (body)
+    return body
+
+for i in range(10):
+
+    x = random.randint(1,10)
+    y = random.randint(1,10)
+    vysledek_zak = int(input(f"{x} * {y} = "))
+    vysledek_zak = int(input(f"{x} / {y} = "))
+    vysledek_zak = int(input(f"{x} + {y} = "))
+    vysledek_zak = int(input(f"{x} ** {y} = "))
+
+    vysledek = nasobeni(x,y) or scitani(x,y) or deleni(x,y) or mocneni(x,y)
+    kontrola(vysledek,vysledek_zak)
+
+print("Koneční body ", body)
